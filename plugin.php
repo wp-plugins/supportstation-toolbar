094c92b2d25cffdedc55350f2a51b1652dbb8caa
DeFries <DeFries@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-02 11:46:35 +0000

    It's all about the meta :)
    git-svn-id: https://plugins.svn.wordpress.org/supportstation-toolbar/tags/1.0@526148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -27,7 +27,8 @@ function fst_support_toolbar_menu() {
 	$wp_admin_bar->add_menu( array( 
 		'id'		=> 'fst_support_toolbar', 
 		'title' 	=> __( 'Support' ), 
-		'href'  	=> 'http://supportstation.org/', 
+		'href'  	=> 'http://supportstation.org/',
+		'meta'        => array( 'target' => '_blank' )
 		)
 	);
 	$wp_admin_bar->add_menu( array( 
@@ -66,7 +67,6 @@ function fst_genesis_toolbar_style() {
 	}
 	#wpadminbar #wp-admin-bar-fst_support_toolbar {
 		background: #87e0fd; /* Old browsers */
-		background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzg3ZTBmZCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjQwJSIgc3RvcC1jb2xvcj0iIzUzY2JmMSIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMwNWFiZTAiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
 		background: -moz-linear-gradient(top,  #87e0fd 0%, #53cbf1 40%, #05abe0 100%); /* FF3.6+ */
 		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#87e0fd), color-stop(40%,#53cbf1), color-stop(100%,#05abe0)); /* Chrome,Safari4+ */
 		background: -webkit-linear-gradient(top,  #87e0fd 0%,#53cbf1 40%,#05abe0 100%); /* Chrome10+,Safari5.1+ */
